9d53fb377ce34bb15bcfb2f23a3ade14b263cb82
Bettyrosew <bettyrosaw@gmail.com>
2019-02-15 15:50:20 +0300

    add changes made to index.html

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function measurements() {
+  var side1=document.getElementById("length1").value;
+  var side2=document.getElementById("length2").value;
+  var side3=document.getElementById("length3").value;
+  var inputArray=[parseInt(side1),parseInt(side2),parseInt(side3)]
+  if(inputArray[0]==inputArray[1]&&inputArray[1]==inputArray[2]) {
+    alert('Equilateral Triangle');
+  }
+}
