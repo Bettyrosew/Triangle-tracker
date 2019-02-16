1833ff1fa847ba13ad2e0a3116d4b3d279639007
Bettyrosew <bettyrosaw@gmail.com>
2019-02-17 00:45:58 +0300

    edit changes in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ function measurements() {
   var side3=document.getElementById("length3").value;
   var inputArray=[parseInt(side1),parseInt(side2),parseInt(side3)]
   if(inputArray[0]==inputArray[1]&&inputArray[1]==inputArray[2]) {
-    alert('Equilateral Triangle');
+    alert('It is an' + ' equilateral triangle');
   }else if(inputArray[0]==inputArray[1]||inputArray[1]==inputArray[2]||inputArray[0]==inputArray[2]) {
-    alert('Isosceles Triangle');
+    alert('It is an'+' isosceles triangle');
   }else if(inputArray[0]+inputArray[1]>inputArray[2] &&inputArray[1]+inputArray[2]>inputArray[0] &&inputArray[0]+inputArray[2]>inputArray[1]) {
-    alert('Scalene Triangle');
+    alert('It is a'+' scalene triangle');
   }else{
     alert('No triangle can be formed');
   }
